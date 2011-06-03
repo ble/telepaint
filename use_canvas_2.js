384195fe165e7c45a792fccc37cfcd7c35b23c9a
Ben Ellis <benjaminster@gmail.com>
2011-06-03 01:04:37 -0400

    Animation + a different op art design

--- use_canvas_2.js
+++ use_canvas_2.js
@@ -14,7 +14,7 @@ var signs = [];
 for(var i = 0; i < 2000; i++) { 
   var sign = i % 3 == 0 ? 1 : -1;
   var radius = 0.0004 * i;
-  var angle = (Math.PI * sign / 3.5);
+  var angle = (Math.PI * sign / 3.35);
   var length = Math.abs(angle) * radius;
   parts.push(new ble.curves.CurvedPart(length, sign / radius));
   signs.push(sign);
@@ -42,9 +42,14 @@ var fullCanvas = new goog.math.Box(0, pxWidth, pxHeight, 0);
 var view = new goog.math.Box(aspect, 1.0, -aspect, -1.0);
 var subcanvas = new ble.scratch.Subcanvas(canvas, fullCanvas, view);
 
-var renderCoords = function(lineWidthMutator, strokeStyle, parts, origin, angle) {
-  var coords = (new ble.curves.Renderer(parts, origin, angle)).renderCurve();
+var renderCoords = function(lineWidthMutator, strokeStyle, parts, origin, angle, coordsPerTime, completer) {
+  var allCoords = (new ble.curves.Renderer(parts, origin, angle)).renderCurve();
+  var coordIndex = 0;
   return function(context) {
+    var coordsThisTime = Math.min(Math.max(0, allCoords.length - coordIndex), coordsPerTime) / 2;
+    var startCoord = Math.max(0, coordIndex - 2);
+    var coords = allCoords.slice(startCoord, coordIndex + 2 * coordsPerTime);
+    coordIndex += 2 * coordsThisTime;
     context.beginPath();
     context.moveTo(coords[0], coords[1]);
     for(var i = 1; i < coords.length / 2; i++) {
@@ -53,25 +58,44 @@ var renderCoords = function(lineWidthMutator, strokeStyle, parts, origin, angle)
     context.lineWidth = lineWidthMutator(context.lineWidth);
     context.strokeStyle = strokeStyle;
     context.stroke();
+    if(coordIndex == allCoords.length)
+      completer.done();
+    console.log(allCoords.length - coordIndex);
   }
 };
 
 var id = function(x) { return x; };
 var theColor = "cdc";
-var render0 = renderCoords(id, theColor, parts, [-0.25, 0.25], 0);
-var render1 = renderCoords(id, theColor, parts, [0.25,-0.25], Math.PI / 6);
-var render2 = renderCoords(id, theColor, parts, [-0.25,-0.25], -Math.PI / 6);
-var render3 = renderCoords(id, theColor, parts, [0.25,0.25], Math.PI / 3);
+
+
+var completer = function() {
+  return {
+    "done": function() {
+      clearInterval(this.id); 
+    },
+    "id": null
+  };
+}
+
+
+
+var completer0 = completer();
+var render0 = renderCoords(id, theColor, parts, [-0.25, 0.25], 0, 10, completer0);
+var id = setInterval(function() { subcanvas.withContext(render0); }, 15);
+completer0.id = id;
+/*
+
+var render1 = renderCoords(id, theColor, parts, [0.25,-0.25], Math.PI / 6, 1000, null);
+var render2 = renderCoords(id, theColor, parts, [-0.25,-0.25], -Math.PI / 6, 1000, null);
+var render3 = renderCoords(id, theColor, parts, [0.25,0.25], Math.PI / 3, 1000, null);
+subcanvas.withContext( render3 );
+subcanvas.withContext( render3 );
+subcanvas.withContext( render3 );
 subcanvas.withContext( render3 );
 subcanvas.withContext( render2 );
+subcanvas.withContext( render2 );
+subcanvas.withContext( render2 );
+subcanvas.withContext( render1 );
 subcanvas.withContext( render1 );
 subcanvas.withContext( render0 );
-
-//var theImg = canvas.element_.toDataURL();
-//var image = domHelper.createDom('img', {"src": theImg});
-//var text = domHelper.createDom('div', null, theImg);
-//var link = domHelper.createDom('a', {"href": theImg}, "clicky clicky" + theImg.length);
-//container.appendChild(image);
-//container.appendChild(link);
-//container.removeChild(canvas.element_);
-//container.appendChild(text);
+*/

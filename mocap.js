24bfbc45af48b96a90c345d6ac0721407a6b4caf
Ben Ellis <benjaminster@gmail.com>
2011-06-12 21:05:12 -0400

    New event-based motion capture class.

--- mocap.js
+++ mocap.js
@@ -1,123 +1,215 @@
-/**
- * @fileoverview Mouse event listening which records time information with
- * coordinates, allowing for replay or analysis of user gestures.
- * @author benjaminster@gmail.com (Ben Ellis)
- */
 
-goog.provide('ble.mocap.PixelMocap');
+
+goog.provide('ble.mocap.Mocap');
 goog.provide('ble.mocap.Replayer');
+goog.provide('ble.mocap.EventType');
 
 goog.require('goog.events');
 goog.require('goog.events.EventType');
+goog.require('goog.events.EventTarget');
+goog.require('goog.events.Event');
 
 /**
- * A motion-capture listener which records the location and time-after-
- * mousedown of every mousemove event after mousedown and before mouseup or
- * mouseout.
- * @param {function(Array.<number>)} callbackDone executed on mouseup / mouseout.
- * @constructor
+ * Constants for event names
+ * @enum {string}
  */
-ble.mocap.PixelMocap = function(callbackMoved, callbackDone) {
-  this.callbackMoved = callbackMoved;
-  this.callbackDone = callbackDone;
-  this.mouseState = this.UP;
-  this.startTime = null;
-  this.coordinates = null;
+ble.mocap.EventType = {
+  BEGIN: 'begin',
+  PROGRESS: 'progress',
+  CONTROLPOINT: 'controlpoint',
+  END: 'end'
 };
 
-ble.mocap.PixelMocap.prototype.DOWN = "down";
-ble.mocap.PixelMocap.prototype.UP = "UP";
-
-ble.mocap.PixelMocap.prototype.handler = function() {
-  return goog.bind(this.handler0, this);
-};
-
-ble.mocap.PixelMocap.prototype.handler0 = function(event) {
-  if(event.type == goog.events.EventType.MOUSEDOWN) {
-    this.initMocap(event);
-    this.mocap(event);
-    this.callbackMoved(event);
-  } else if(this.mouseState == this.DOWN &&
-            (event.type == goog.events.EventType.MOUSEUP ||
-             event.type == goog.events.EventType.MOUSEOUT)) {
-    this.mocap(event);
-    this.callbackMoved(event);
-    var coords = this.coordinates.slice();
-    this.terminateMocap();
-    this.callbackDone(coords);
-  } else if(this.mouseState == this.DOWN &&
-            event.type == goog.events.EventType.MOUSEMOVE) {
-    this.mocap(event);
-    this.callbackMoved(event);
-  }
-}
-
-ble.mocap.PixelMocap.prototype.initMocap = function(event) {
-  this.mouseState = this.DOWN;
-  this.startTime = event.getBrowserEvent().timeStamp;
-  this.coordinates = [];
-}
-
-ble.mocap.PixelMocap.prototype.mocap = function(event) {
-  var delta = event.getBrowserEvent().timeStamp - this.startTime;
-  this.coordinates.push(delta, event.offsetX, event.offsetY);
-}
-
-ble.mocap.PixelMocap.prototype.terminateMocap = function() {
-  this.mouseState = this.UP;
-  this.startTime = null;
-  this.coordinates = null;
-}
-
-/**
- * @param {Array.<number>} capture (time, x, y) triplets
- * @param {function(Array.<number>)} callback to call when replaying
- * @constructor
- */
-
-ble.mocap.Replayer = function(capture, callback) {
-  this.capture = capture.slice();
-  if(capture.length % 3 != 0)
-    throw Error('capture must be array of t, x, y triplets');
-  this.callback = callback;
-  this.timeStart = null;
-  this.lastIndex = null;
-  this.started = false;
-  this.id = null;
-}
-
-ble.mocap.Replayer.prototype.delay = 10; //milliseconds
-
-ble.mocap.Replayer.prototype.start = function() {
-  if(this.started)
-    throw Error('replay already started');
-  this.started = true;
-  this.timeStart = (new Date()).getTime();
-  this.lastIndex = 0;
-  this.update = goog.bind(this.update0, this);
-  this.id = this.scheduleReplay(this.delay);
-}
-
-ble.mocap.Replayer.prototype.scheduleReplay = function(delay) {
-  return window.setTimeout(this.update, delay);
-}
-
-ble.mocap.Replayer.prototype.update0 = function() {
-  var delta = (new Date()).getTime() - this.timeStart;
-  var index = this.lastIndex + 1;
-  while(index < this.capture.length / 3 && this.capture[index * 3] < delta) 
-    index++;
-  var coords = [];
-  for(var ix = this.lastIndex; ix < index; ix++) {
-    coords.push(this.capture[ix * 3 + 1], this.capture[ix * 3 + 2]);
-  }
-  this.lastIndex = index - 1;
-  this.callback(coords);
-  if(!(this.lastIndex == this.capture.length / 3 - 1))
-    this.id = this.scheduleReplay(this.delay);
+ble.mocap.EventType.ALL =
+  [ble.mocap.EventType.BEGIN,
+  ble.mocap.EventType.PROGRESS,
+  ble.mocap.EventType.CONTROLPOINT,
+  ble.mocap.EventType.END];
   
+
+/**
+ * A motion-capture object.
+ * @param {number} startTime
+ * @constructor
+ */
+ble.mocap.Capture = function(startTime) {
+  this.startTime = startTime;
+  this.coordinates = [];
+  this.times = [];
+  this.controlTimeIndices = [];
+  this.controlPoints = [];
 }
 
+/**
+ * Concatenate a regular point to the capture.
+ * @param {number} time
+ * @param {number} x
+ * @param {number} y
+ */
+ble.mocap.Capture.prototype.addCoordinates = function(time, x, y) {
+  this.times.push(time - this.startTime);
+  this.coordinates.push(x, y);
+}
 
+/**
+ * Concatenate a control point to the capture.
+ * @param {number} time
+ * @param {number} x
+ * @param {number} y
+ * @param {*} controlValue
+ */
+ble.mocap.Capture.prototype.addControl = function(time, x, y, controlValue) {
+  this.times.push(time - this.startTime);
+  this.coordinates.push(x, y);
+  this.controlTimeIndices.push(this.times.length - 1);
+  this.controlPoints.push(controlValue);
+}
 
+/**
+ * Time-slice the capture.
+ * @param {number} time0
+ * @param {number} time1
+ * @return {Array.<number>}
+ */
+ble.mocap.Capture.prototype.betweenTimes = function(time0, time1) {
+  var ix0 = Math.ceil(ble.mocap.binarySearch(this.times, time0));
+  var ix1 = Math.floor(ble.mocap.binarySearch(this.times, time1));
+  var cIx0 = Math.ceil(ble.mocap.binarySearch(this.controlTimeIndices, ix0));
+  var cIx1 = Math.floor(ble.mocap.binarySearch(this.controlTimeIndices, ix1));
+  return [ix0, ix1, cIx0, cIx1];
+}
 
+ble.mocap.binarySearch = function(array, value) {
+  if(array.length == 0)
+    return -1;
+  return ble.mocap.binarySearch0(array, 0, array.length-1, Math.floor(array.length / 2), value);
+};
+
+ble.mocap.binarySearch0 = function(array, ix0, ixF, ix, value) {
+  var ixVal = array[ix];
+  if(ixVal == value) {
+    return ix;
+  }
+  
+  else if(ix0 == ix || ixF == ix) {
+    if(ixVal > value)
+      return ix - 0.5;
+    if(ixVal < value)
+      return ix + 0.5;
+  }
+
+  else if(ixVal > value) {
+    return ble.mocap.binarySearch0(array, ix0, ix, Math.floor((ix0 + ix) / 2), value);
+  }  else { 
+    return ble.mocap.binarySearch0(array, ix, ixF, Math.ceil((ix + ixF) / 2), value);
+  }
+}
+
+/**
+ * Abstract class providing motion-capture functionality for mouse events.
+ * @constructor
+ */
+
+ble.mocap.Mocap = function() {
+  this.startTime = null;
+  this.capture = null;
+  /**
+   * Targets which will receive mocap events.
+   * @type {Object.<string,Array.<goog.events.EventTarget>}
+   */
+  this.targets = {};
+}
+
+ble.mocap.Mocap.prototype.dispatchMocap = function(type) {
+  var targets = this.targets[type];
+  if(goog.isNull(targets))
+    return;
+  var dispatchMocap = new goog.events.Event(type);
+  dispatchMocap.capture = this.capture;
+  for(var i = 0; i < targets.length; i++) {
+    var result = targets[i].dispatchEvent(dispatchMocap);
+    if(result === false)
+      return;
+  }
+}
+
+ble.mocap.Mocap.prototype.forwardingListener = goog.abstractMethod;
+
+/**
+ * @param {goog.events.EventTarget} target
+ * @param {string} type
+ */
+ble.mocap.Mocap.prototype.addTarget = function(target, type) {
+  if(goog.isArray(type)) {
+    for(var i = 0; i < type.length; i++)
+      this.addTarget(target, type[i]);
+  } else {
+    if(!(type in this.targets))
+      this.targets[type] = [];
+    var targets = this.targets[type];
+    for(var i = 0; i < targets.length; i++) {
+      if(targets[i] === target)
+        return;
+    }
+    targets.unshift(target);
+  }
+}
+
+ble.mocap.Mocap.prototype.beginCapture = function(event) {
+  var time = event.getBrowserEvent().timeStamp;
+  this.capture = new ble.mocap.Capture(time);
+  this.capture.addCoordinates(time, event.offsetX, event.offsetY);
+  this.dispatchMocap(ble.mocap.EventType.BEGIN);
+}
+
+ble.mocap.Mocap.prototype.progressCapture = function(event) {
+  var time = event.getBrowserEvent().timeStamp;
+  this.capture.addCoordinates(time, event.offsetX, event.offsetY);
+  this.dispatchMocap(ble.mocap.EventType.PROGRESS);
+}
+
+ble.mocap.Mocap.prototype.controlCapture = function(event, controlValue) {
+  var time = event.getBrowserEvent().timeStamp;
+  this.capture.addControl(time, event.offsetX, event.offsetY, controlValue);
+  this.dispatchMocap(ble.mocap.EventType.CONTROLPOINT);
+}
+
+ble.mocap.Mocap.prototype.endCapture = function(event) {
+  var time = event.getBrowserEvent().timeStamp;
+  this.capture.addCoordinates(time, event.offsetX, event.offsetY);
+  this.dispatchMocap(ble.mocap.EventType.END);
+  this.capture = null;
+}
+
+/**
+ * Stroke-based motion capture: capture begins on mousedown, progresses with
+ * each mousemove until a mouseup event.
+ * @constructor
+ * @extends {ble.mocap.Mocap}
+ */
+ble.mocap.Stroke = function() {
+  ble.mocap.Mocap.call(this);
+  this.midStroke = false;
+};
+goog.inherits(ble.mocap.Stroke, ble.mocap.Mocap);
+
+ble.mocap.Stroke.prototype.forwardingListener = function(event) {
+  if(this.midStroke) {
+    if(event.type === goog.events.EventType.MOUSEMOVE) {
+      this.progressCapture(event);
+    } else if(event.type === goog.events.EventType.MOUSEUP ||
+              event.type === goog.events.EventType.MOUSEOUT) {
+      this.endCapture(event);
+      this.midStroke = false;
+    }
+  } else if(event.type === goog.events.EventType.MOUSEDOWN) {
+    this.midStroke = true;
+    this.beginCapture(event);
+  } 
+}
+
+ble.mocap.Stroke.prototype.eventTypesOfInterest =
+  [goog.events.EventType.MOUSEDOWN,
+   goog.events.EventType.MOUSEMOVE,
+   goog.events.EventType.MOUSEUP,
+   goog.events.EventType.MOUSEOUT];
--- use_canvas_3.js
+++ use_canvas_3.js
@@ -1,9 +1,86 @@
 goog.require('ble.scratch.Canvas');
-goog.require('ble.mocap.PixelMocap');
+goog.require('ble.mocap.Stroke');
 goog.require('goog.events');
 goog.require('goog.events.EventType');
 
+//Scene graph
+var scene = {
+  beingDrawn: null,
+  beingReplayed: null,
+  startTimes: null,
+  complete: []
+};
 
+//Utility drawing
+var strokeCoords = function(ctx, coords) {
+  if(coords.length == 0)
+    return;
+  ctx.beginPath();
+  ctx.moveTo(coords[0], coords[1]);
+  for(var i = 1; i < coords.length / 2; i++)
+    ctx.lineTo(coords[2*i], coords[2*i+1]);
+  ctx.stroke();
+}
+
+var strokeCoordsWithin = function(ctx, coords, start, last) {
+  if(coords.length == 0)
+    return;
+  ctx.beginPath();
+  ctx.moveTo(coords[2*start], coords[2*start+1]);
+  for(var i = start+1; i <= last; i++)
+    ctx.lineTo(coords[2*i], coords[2*i+1]);
+  ctx.stroke();
+}
+
+
+//Redraw strokes at various stages of progress
+var redraw = function(ctx) {
+  var now = Date.now();
+  ctx.clearRect(0, 0, pxWidth, pxHeight);
+  if(!goog.isNull(scene.beingDrawn)) {
+    strokeCoords(ctx, scene.beingDrawn);
+  }
+  for(var i = 0; i < scene.complete.length; i++) {
+    strokeCoords(ctx, scene.complete[i]);
+  }
+  if(!goog.isNull(scene.beingReplayed)) {
+    var replayRemaining = [];
+    var startTimeRemaining = [];
+    while(scene.beingReplayed.length > 0) {
+      var replay = scene.beingReplayed.pop();
+      var startTime = scene.startTimes.pop();
+      var timeDelta = now - startTime;
+      var indices = replay.betweenTimes(0, timeDelta);
+      strokeCoordsWithin(ctx, replay.coordinates, indices[0], indices[1]);
+      var lastTime = replay.times[replay.times.length - 1];
+      if(timeDelta <= lastTime) {
+        replayRemaining.unshift(replay);
+        startTimeRemaining.unshift(startTime);
+      } else {
+        scene.complete.unshift(replay.coordinates);
+      }
+    }
+    if(replayRemaining.length > 0) {
+      scene.beingReplayed = replayRemaining;
+      scene.startTimes = startTimeRemaining;
+    } else {
+      scene.beingReplayed = null;
+      scene.startTimes = null;
+    }
+  }
+}
+
+var delay = 10;
+
+var scheduleUpdate = function() {
+  return window.setTimeout(update, delay);
+}
+var update = function() {
+  canvas.withContext(redraw);
+  if(!goog.isNull(scene.beingReplayed)) {
+    scheduleUpdate();
+  }
+}
 
 var pxWidth = 640;
 var pxHeight = 480;
@@ -14,27 +91,38 @@ var container = domHelper.getElement("outermost");
 var canvas = new ble.scratch.Canvas(pxWidth, pxHeight);
 canvas.render(container);
 
-//var replayCallback = goog.bind(console.log, console);
-var replayCallback = function(coords) {
-  canvas.withContext(function(context) {
-    context.beginPath();
-    context.moveTo(coords[0], coords[1]);
-    while(coords.length >= 2) {
-      context.lineTo(coords.shift(), coords.shift());
+
+var mocapHandler = new goog.events.EventTarget();
+
+mocapHandler.handler0 = function(event) {
+  if(event.type == ble.mocap.EventType.BEGIN) {
+    scene.beingDrawn = event.capture.coordinates;
+    canvas.withContext(redraw);
+  } else if(event.type == ble.mocap.EventType.PROGRESS ||
+            event.type == ble.mocap.EventType.CONTROLPOINT) {
+    canvas.withContext(redraw);
+  } else if(event.type == ble.mocap.EventType.END) {
+    scene.beingDrawn = null;
+    if(goog.isNull(scene.beingReplayed)) {
+      scene.beingReplayed = [];
+      scene.startTimes = [];
     }
-    context.stroke();
-  });
-}
+    scene.beingReplayed.push(event.capture);
+    scene.startTimes.push(Date.now() - 50);
+    update();
+  }
+};
 
-var mocapCallback = function(capture) {
-  var replayer = new ble.mocap.Replayer(capture, replayCallback);
-  replayer.start();
-}
-var mocap = new ble.mocap.PixelMocap(mocapCallback);
-var relevantTypes = 
-  [goog.events.EventType.MOUSEDOWN,
-   goog.events.EventType.MOUSEUP,
-   goog.events.EventType.MOUSEOUT,
-   goog.events.EventType.MOUSEMOVE];
+var motionCapture = new ble.mocap.Stroke();
+goog.events.listen(
+    canvas.getElement(),
+    motionCapture.eventTypesOfInterest,
+    motionCapture.forwardingListener,
+    false,
+    motionCapture);
 
-goog.events.listen(canvas.element_, relevantTypes, mocap.handler());
+motionCapture.addTarget(mocapHandler, ble.mocap.EventType.ALL);
+goog.events.listen(
+    mocapHandler,
+    ble.mocap.EventType.ALL,
+    mocapHandler.handler0);

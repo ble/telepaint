7a064dd9af14034c22f08a7217a0ff14c879fb5a
Ben Ellis <benjaminster@gmail.com>
2011-08-14 12:52:37 -0400

    Minor cleanup

--- server/src/blizzard_resource.erl
+++ server/src/blizzard_resource.erl
@@ -27,37 +27,4 @@ to_json(Req, stateless) ->
       {jiffy:encode(Eson), Req, stateless};
     {error, Reason} ->
       {io_lib:format("'~p'", [Reason]), Req, stateless}
-  end.
-% 
-%get_snowflake(Req) ->
-%  Dp = wrq:disp_path(Req),
-%  Pir = wrq:path_info(snowflake_id, Req),
-%  io:format("disp_path -> ~p~npath_info(snowflake_id) -> ~p~n", [Dp, Pir]),
-%  case wrq:disp_path(Req) of
-%    [] ->
-%      case wrq:path_info(snowflake_id, Req) of
-%        undefined ->
-%          no_snowflake;
-%        SnowflakeId0 ->
-%          SnowflakeId = erlang:list_to_binary(SnowflakeId0),
-%          io:format("Requested snowflake id: ~p~n", [SnowflakeId]),
-%          case mnesia:transaction(fun() -> mnesia:read({snowflake, SnowflakeId}) end) of
-%            {atomic, Result} ->
-%              Result;
-%            _ ->
-%              error
-%          end
-%      end;
-%    _ ->
-%      bad_path
-%  end.
-%
-%get_session_id(Req) ->
-%  wrq:get_cookie_value("session", Req).
-%
-%assemble_json(Snowflake, Fragments) ->
-%  io:format("foo~n", []),
-%  FragmentJson = [F#snowflake_fragment.json || F <- Fragments],
-%  Eson = {[{<<"id">>, Snowflake#snowflake.id}, {<<"fragments">>, FragmentJson}]},
-%  io:format("eson output: ~p~n", [Eson]),
-%  jiffy:encode(Eson).
+  end.  

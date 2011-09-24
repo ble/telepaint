36209c49adb1842f63df4df5ef0fd65dddb7b480
Ben Ellis <benjaminster@gmail.com>
2011-09-24 00:57:56 -0400

    add extern definition for webkitRequestAnimationFrame

--- config-3.js
+++ config-3.js
@@ -3,5 +3,6 @@
   "paths": ".",
   "mode": "SIMPLE",
   "level": "VERBOSE",
-  "inputs": ["subcanvas.js", "mocap.js", "gfx.js", "use_canvas_3.js"]
+  "inputs": ["subcanvas.js", "mocap.js", "gfx.js", "use_canvas_3.js"],
+  "externs": ["extern_webkitRAF.js"]
 }
---
+++ extern_webkitRAF.js
@@ -0,0 +1,5 @@
+/**
+ * @param {function(number)} cb callback taking posix millis time of next anim frame
+ * @param {Element=} elt element whose visibility is tested before making callback
+ */
+Window.prototype.webkitRequestAnimationFrame = function(cb, elt) {};

955639d11702c55439a3b6f2a420a6565171616b
Ben Ellis <benjaminster@gmail.com>
2011-10-06 02:49:49 -0400

    tiny cleanups

--- config-3.js
+++ config-3.js
@@ -3,6 +3,6 @@
   "paths": ".",
   "mode": "ADVANCED",
   "level": "VERBOSE",
-  "inputs": ["subcanvas.js", "mocap.js", "gfx.js", "use_canvas_3.js", "replay.js", "icon.js", "path.js"],
+  "inputs": ["subcanvas.js", "scribble.js", "mocap.js", "gfx.js", "use_canvas_3.js", "replay.js", "icon.js", "path.js"],
   "externs": ["extern_webkitRAF.js"]
 }

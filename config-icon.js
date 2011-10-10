f1d10a9a001dac28d909ffb47fd8c8c3c49039bc
Ben Ellis <benjaminster@gmail.com>
2011-10-09 21:06:07 -0400

    start on style picker

---
+++ config-icon.js
@@ -0,0 +1,8 @@
+{
+  "id": "icon",
+  "paths": ".",
+  "mode": "ADVANCED",
+  "level": "VERBOSE",
+  "inputs": ["subcanvas.js", "scribbles.js", "mocap.js", "gfx.js", "replay.js", "icon.js", "path.js", "json.js"],
+  "externs": ["extern_webkitRAF.js"]
+}
--- icon.js
+++ icon.js
@@ -1,6 +1,7 @@
 goog.require('goog.dom.DomHelper');
 goog.require('goog.math.Box');
 goog.require('goog.events.EventTarget');
+goog.require('goog.ui.HsvaPalette');
 
 goog.require('ble.scratch.Canvas');
 goog.require('ble.scratch.Subcanvas');
@@ -11,6 +12,99 @@ goog.provide('ble.scribble.icon');
 
 goog.provide('ble.scribble.icon.makeNormalizedStrokeRecorder');
 goog.provide('ble.scribble.icon.makeNormalizedPolylineRecorder');
+goog.provide('ble.scribble.icon.StylePicker');
+
+ble.scribble.icon.backdropOn = function(ctx, width, height, blockSize) {
+  ctx.save();
+  var wCount = Math.ceil(width / blockSize);
+  var hCount = Math.ceil(height / blockSize);
+  var color0 = "#bbb";
+  var color1 = "#888";
+  for(var i = 0; i < wCount; i++) {
+    for(var j = 0; j < hCount; j++) {
+      var x = i * blockSize;
+      var y = j * blockSize;
+      ctx.beginPath();
+      ctx.rect(x, y, blockSize, blockSize);
+      ctx.fillStyle = ((i + j) % 2 == 0) ? color0 : color1;
+      ctx.fill();
+    }
+  }
+  ctx.restore();
+};
+
+/**
+ * @constructor
+ * @extends {goog.ui.Component}
+ */
+ble.scribble.icon.StylePicker = function() {
+  goog.ui.Component.call(this);
+};
+goog.inherits(ble.scribble.icon.StylePicker, goog.ui.Component);
+
+
+ble.scribble.icon.StylePicker.prototype.height = 187;
+
+/**
+ * Disallow deocartion.
+ * @override
+ */
+ble.scribble.icon.StylePicker.prototype.canDecorate = function(element) {
+  return false;
+};
+
+ble.scribble.icon.StylePicker.prototype.createDom = function() {
+  var domHelper = this.getDomHelper();
+  var container = domHelper.createDom('div', {'class': 'ble-scribble-stylepicker'});
+  this.setElementInternal(container);
+
+  this.bigIcon = new ble.scratch.Canvas(this.height, this.height);
+  this.addChild(this.bigIcon, true);
+
+  var smallSize = Math.floor(this.height / 2);
+  var smallIconContainer = new goog.ui.Component();
+  var height = this.height;
+  smallIconContainer.createDom = function() {
+    var domHelper = this.getDomHelper();
+    var elt = domHelper.createElement("div");
+    elt.style['width'] = height; 
+    elt.style['height'] = height; 
+    elt.style['display'] = "inline-block";
+//    elt.style['margin'] = 0;
+//    elt.style['padding'] = 0;
+    this.setElementInternal(elt);
+  };
+  //replace manual styling with css class plz
+  this.smallIcons = [];
+  for(var i = 0; i < 4; i++) {
+    var smallIcon = new ble.scratch.Canvas(smallSize, smallSize);
+    smallIconContainer.addChild(smallIcon, true);
+    this.smallIcons.push(smallIcon);
+  };
+  this.smallIconContainer = smallIconContainer;
+  this.addChild(smallIconContainer, true);
+
+  this.hsva1 = new goog.ui.HsvaPalette(domHelper, '#000', 1.0, 'goog-hsva-palette-sm');
+  this.addChild(this.hsva1, true);
+
+  this.hsva2 = new goog.ui.HsvaPalette(domHelper, '#ffc', 1.0, 'goog-hsva-palette-sm'); 
+  this.addChild(this.hsva2, true);
+};
+
+ble.scribble.icon.StylePicker.prototype.enterDocument = function() {
+  this.bigIcon.withContext(function(ctx) {
+    ble.scribble.icon.backdropOn(ctx, 187, 187, 31);
+  });
+
+  
+
+  for(var i = 0; i < this.smallIcons.length; i++) {
+    var smallIcon = this.smallIcons[i];
+    smallIcon.withContext(function(ctx) {
+      ble.scribble.icon.backdropOn(ctx, 93, 93, 16);
+    });
+  }
+};
 /**
  * @constructor
  * @extends {goog.events.EventTarget}

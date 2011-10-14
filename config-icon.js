87c7c970009af770e947c78c99588dff17698dc8
Ben Ellis <benjaminster@gmail.com>
2011-10-13 20:14:33 -0400

    add motion captures for icon drawing

--- config-icon.js
+++ config-icon.js
@@ -3,6 +3,6 @@
   "paths": ".",
   "mode": "ADVANCED",
   "level": "VERBOSE",
-  "inputs": ["subcanvas.js", "scribbles.js", "mocap.js", "gfx.js", "replay.js", "stylepicker.js", "path.js", "json.js"],
+  "inputs": ["icon_caps.js", "subcanvas.js", "scribbles.js", "mocap.js", "gfx.js", "replay.js", "stylepicker.js", "path.js", "json.js"],
   "externs": ["extern_webkitRAF.js"]
 }

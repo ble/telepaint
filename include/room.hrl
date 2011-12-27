eeb47141bc808851bdc1543b83ce444d4d4f9fab
Ben Ellis <benjaminster@gmail.com>
2011-12-27 01:05:43 -0500

    Thoughts about live/referential persist/view persist

--- include/room.hrl
+++ include/room.hrl
@@ -14,3 +14,22 @@
 
 -record(game,
   {id, start_stamp, end_stamp, room_id, stack_ids, player_ids}).
+
+
+%so if we ignore persistence entirely, what's the right model?
+
+-record(game,
+  {id, start_stamp, players, stacks_by_player}).
+
+-record(player,
+  {id, name, pid}).
+
+-record(stack,
+  {id, drawings}).
+
+-record(drawing,
+  {id, start_stamp, player_id, data}).
+
+%Now that might not be the right model, but that is revealing.
+%With persistence I'm leaning towards ids and a relational thang.
+%With game state, I'm leaning towards containment relationships.

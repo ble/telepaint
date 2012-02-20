dbe8980688908f6595029286f6965900c9db3bc5
Ben Ellis <benjaminster@gmail.com>
2012-02-19 21:53:30 -0500

    Json RPC shizzzle

---
+++ src/json_rpc.erl
@@ -0,0 +1,102 @@
+-module(json_rpc).
+-export([unjif/1, jif/1]).
+-export_records([rpc_call, rpc_response, rpc_response_error]).
+
+-record(rpc_call,
+  { version = <<"2.0">>,
+    method = <<"unspecified_method">>,
+    params = [],
+    id = undefined
+  }).
+
+-record(rpc_response_error,
+  { code = -1,
+    message = <<"undescribed error">>,
+    data = none
+  }).
+
+-record(rpc_response,
+  { version = <<"2.0">>,
+    result = undefined,
+    error = undefined,
+    id = undefined
+  }).
+
+
+jif_obj(X) ->
+  {jif_obj_(X)}.
+
+jif_obj_([])                   -> [];
+jif_obj_([{_A, undefined} | X]) -> jif_obj_(X);
+jif_obj_([{A, B} | X])         -> [{A, B} | jif_obj_(X)].
+
+maybe_jif(X) ->
+  try jif(X) catch error:_ -> X end.
+
+jif(undefined) -> undefined;
+jif(#rpc_call{version = V, method = M, params = P, id = I}) ->
+  jif_obj([
+    {<<"version">>, V},
+    {<<"method">>, M},
+    {<<"params">>, rpc_methods:unpopulate(P)},
+    {<<"id">>, I}]);
+
+jif(#rpc_response_error{code = C, message = M, data = D}) ->
+  jif_obj([
+    {<<"code">>, C},
+    {<<"message">>, M},
+    {<<"data">>, D}]);
+
+jif(#rpc_response{version = V, result = R, error = undefined, id = I})
+    when R =/= undefined ->
+  jif_obj([
+   {<<"version">>, V},
+   {<<"result">>, maybe_jif(R)},
+   {<<"id">>, I}]);
+
+
+jif(#rpc_response{version = V, result = undefined, error = E, id = I})
+    when E =/= undefined ->
+  jif_obj([
+    {<<"version">>, V},
+    {<<"error">>, maybe_jif(E)},
+    {<<"id">>, I}]).
+
+unjif({PropList}) ->
+  unjif_(PropList).
+
+unjif_(PropList) ->
+  Version = proplists:get_value(<<"version">>, PropList),
+  Method = proplists:get_value(<<"method">>, PropList),
+  Id = proplists:get_value(<<"id">>, PropList),
+  Params = rpc_methods:populate(Method, proplists:get_value(<<"params">>, PropList)),
+  #rpc_call{
+    version = Version,
+    method = Method,
+    id = Id,
+    params = Params
+  }.
+
+maybe_unjif(_Method, X) -> X.
+
+
+%-record(rpc_call,
+%  { version = <<"2.0">>,
+%    method = <<"unspecified_method">> :: binary(),
+%    params = [] :: [term()],
+%    id = undefined :: undefined | binary() | integer()
+%  }).
+
+%-record(rpc_response,
+%  { version = <<"2.0">>,
+%    result = undefined :: term(),
+%    error = undefined :: term(),
+%    id = undefined :: undefined | binary() | integer()
+%  }).
+
+%-record(rpc_response_error,
+%  { code = -1 :: integer(),
+%    message = <<"undescribed error">> :: binary(),
+%    data = none :: undefined | term()
+%  }).
+
---
+++ src/rpc_methods.erl
@@ -0,0 +1,43 @@
+-module(rpc_methods).
+
+-compile({parse_transform, exprecs}).
+-compile(export_all).
+-export_records([set_name, chat]).
+
+-record(set_name,
+  { who = self,
+    name = undefined }).
+
+-record(chat,
+  { who = self,
+    message = undefined }).
+
+bin_to_atom(Binary) ->
+  try
+    erlang:binary_to_existing_atom(Binary, utf8)
+  catch
+    error:badarg -> undefined
+  end.
+
+
+set_bin({BinKey, Value}, Record) ->
+  Atom = bin_to_atom(BinKey),
+  case Atom of
+    undefined -> Record;
+    _ -> '#set-'([{Atom, Value}], Record)
+  end.
+
+populate(Method, {PropList}) ->
+  Atom = bin_to_atom(Method),
+  StartRecord = '#new-'(Atom),
+  lists:foldl(
+    fun set_bin/2, 
+    StartRecord,
+    PropList).
+
+unpopulate(Record) ->
+  RecordName = element(1, Record),
+  Fields = '#info-'(RecordName),
+  { [{ atom_to_binary(Field, utf8),
+       '#get-'(Field, Record) } || Field <- Fields]}.
+

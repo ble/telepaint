d8d088a6ec6840ff66ff801129439354fb77e76e
Ben Ellis <benjaminster@gmail.com>
2012-02-24 19:45:11 -0500

    Skeleton for comet functionality on server

---
+++ src/observer_queue_resource.erl
@@ -0,0 +1,19 @@
+-module(observer_queue_resource).
+-include("room_context.hrl").
+
+-export([
+%    init/1
+%   ,service_available/2
+%   ,allowed_methods/2
+%   ,resource_exists/2
+%   ,forbidden/2
+%
+%   ,content_types_provided/2
+%   ,to_json/2
+ ]).
+
+
+out_of_date(_, _) -> false.
+
+init([]) ->
+  {ok, #room_context{}}.

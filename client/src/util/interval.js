9a4266af66a7d8e5dd4bc375d93db21ca2013a50
Ben Ellis <benjaminster@gmail.com>
2012-03-11 10:30:25 -0400

    Use "module name" style, add withoutGapsFromZero, trash Tweakers

--- client/src/util/interval.js
+++ client/src/util/interval.js
@@ -1,6 +1,7 @@
 goog.require('ble.util.isSortedBy');
 goog.require('ble.util.comparatorFromRank');
 
+goog.provide('ble.interval');
 goog.provide('ble.interval.Interval');
 goog.provide('ble.interval.AdjustableInterval');
 goog.provide('ble.interval.Impl');
@@ -12,171 +13,145 @@ goog.provide('ble.interval.Fetcher');
 goog.provide('ble.interval.startRank');
 goog.provide('ble.interval.endRank');
 
+////////////////////////////////////////////////////////////////////////////////
+                                                        goog.scope(function(){
+////////////////////////////////////////////////////////////////////////////////
+var _ = ble.interval;
 /**
  * @interface
  */
-ble.interval.Interval = function() {};
+_.Interval = function() {};
 
 /**
  * @return {number}
  */
-ble.interval.Interval.prototype.start = function() {};
+_.Interval.prototype.start = function() {};
 
 /**
  * @return {number}
  */
-ble.interval.Interval.prototype.end = function() {};
+_.Interval.prototype.end = function() {};
 
 /**
  * @return {number}
  */
-ble.interval.Interval.prototype.length = function() {};
+_.Interval.prototype.length = function() {};
 
 /**
  * @interface
  * @extends {ble.interval.Interval}
  */
-ble.interval.AdjustableInterval = function() {};
+_.AdjustableInterval = function() {};
 
 /**
  * @param {number} newStart
  * @return {ble.interval.AdjustableInterval}
  */
-ble.interval.AdjustableInterval.prototype.withStartAt = function(newStart) {};
+_.AdjustableInterval.prototype.withStartAt = function(newStart) {};
 
 /**
  * @param {number} newLength
  * @return {ble.interval.AdjustableInterval}
  */
-ble.interval.AdjustableInterval.prototype.withLength = function(newLength) {};
+_.AdjustableInterval.prototype.withLength = function(newLength) {};
 
 /**
  * @constructor
  * @implements {ble.interval.AdjustableInterval}
  */
-ble.interval.Impl = function(start, end) {
+_.Impl = function(start, end) {
   this.start_ = start;
   this.end_ = end;
 };
 
-ble.interval.Impl.prototype.start = function() { return this.start_; };
+_.Impl.prototype.start = function() { return this.start_; };
 
-ble.interval.Impl.prototype.end = function() { return this.end_; };
+_.Impl.prototype.end = function() { return this.end_; };
 
-ble.interval.Impl.prototype.length = function() { return this.end_ - this.start_; };
-
-/**
- * @override
- * @return {ble.interval.Impl}
- */
-ble.interval.Impl.prototype.withStartAt = function(newStart) {
-  return new ble.interval.Impl(newStart, newStart + this.length());
-};
+_.Impl.prototype.length = function() { return this.end_ - this.start_; };
 
 /**
  * @override
  * @return {ble.interval.Impl}
  */
-ble.interval.Impl.prototype.withLength = function(newLength) {
-  return new ble.interval.Impl(this.start(), this.start() + newLength);
-};
-
-
-ble.interval.startRank = function(x) { return x.start(); };
-ble.interval.endRank = function(x) { return x.end(); };
-
-/**
- * @constructor
- * @abstract
- */
-ble.interval.Tweaker = function() {};
-
-/**
- * @param {Array.<ble.interval.AdjustableInterval>} intervals
- * @return {Array.<ble.interval.AdjustableInterval>}
- */
-ble.interval.Tweaker.prototype.tweakedIntervals = function(intervals) {
-  var plan = this.prepare_(intervals);
-  var result = [];
-  for(var i = 0; i < intervals.length; i++) {
-    var altered = this.altered_(plan, i);
-    result.push(altered);
-  }
-  return result;
+_.Impl.prototype.withStartAt = function(newStart) {
+  return new _.Impl(newStart, newStart + this.length());
 };
 
 /**
- * @param {Array.<ble.interval.AdjustableInterval>} intervals
- * @return {Object}
+ * @override
+ * @return {ble.interval.Impl}
  */
-ble.interval.Tweaker.prototype.prepare_ = goog.abstractMethod;
-
-/**
- * @param {Object} plan
- * @param {number} index
- * @return {ble.interval.AdjustableInterval}
- */
-ble.interval.Tweaker.prototype.altered_ = goog.abstractMethod;
-
-/**
- * @constructor
- * @extends {ble.interval.Tweaker}
- */
-ble.interval.NoOverlap = function() {};
-goog.inherits(ble.interval.NoOverlap, ble.interval.Tweaker);
-
-ble.interval.NoOverlap.prototype.prepare_ = function(intervals) {
-  var plan;  
-  if(ble.util.isSortedBy(ble.interval.startRank, intervals)) {
-    plan = intervals;
-  } else {
-    plan = intervals.slice(0);
-    plan.sort(ble.util.comparatorFromRank(ble.interval.startRank));
-  }
-  return plan;
+_.Impl.prototype.withLength = function(newLength) {
+  return new _.Impl(this.start(), this.start() + newLength);
 };
 
-ble.interval.NoOverlap.prototype.altered_ = function(plan, index) {
-  if(index == 0) {
-    return plan[index];
-  } else {
-    var interval = plan[index];
-    var lastInterval = plan[index - 1];
-    if(interval.start() < lastInterval.end()) {
-      return interval.withStartAt(lastInterval.end());
-    } else {
-      return interval;
+_.union = function(x, y) {
+  return new _.Impl(
+      Math.min(x.start(), y.start()),
+      Math.max(x.end(), y.end()));
+};
+
+_.overlaps = function(x, y) {
+  var xS = x.start(), xE = x.end();
+  var yS = y.start(), yE = y.end();
+         //one or both of x's endpoints are in y
+  return (xS <= yE && xS >= yS) || 
+         (xE <= yE && xE >= yS) ||
+         //if x and y overlap but neither of x's endpoints are in y, then
+         //y must be entirely contained in x.
+         (xS < yS && xE > yE);
+};
+
+_.withoutGapsFromZero = function(startSortedIntervals) {
+  var intervals = startSortedIntervals.slice();
+  var intervalsOut = [];
+  var end = 0;
+
+//%if we just wanted no gaps and didn't care about starting from zero...
+//  if(intervals.length > 0)
+//    end = intervals[0].start;
+
+  while(intervals.length > 0) {
+
+    //Get the next interval, align it without a gap
+    var itemIn = intervals.shift();
+    intervalsOut.push(itemIn.withStartAt(end));
+
+    var span = itemIn;
+    var spanStart = span.start();
+
+    while(intervals.length > 0 && _.overlaps(span, intervals[0])) {
+      var overlappedItem = intervals.shift();
+
+      //Grow the interval covered by the overlapping intervals
+      span = _.union(span, overlappedItem);
+
+      //Set the start to the correct offset
+      var newStart = end + overlappedItem.start() - spanStart;
+      intervalsOut.push(overlappedItem.withStartAt(newStart));
     }
+
+    //The next interval will begin where the set of overlapping intervals
+    //ended.
+    end += span.length();
   }
+  return intervalsOut;
+
 };
 
-/**
- * @constructor
- * @extends {ble.interval.Tweaker}
- */
-ble.interval.Gapless = function() {};
-goog.inherits(ble.interval.Gapless, ble.interval.Tweaker);
-
-ble.interval.Gapless.prototype.prepare_ = ble.interval.NoOverlap.prototype.prepare_;
-ble.interval.Gapless.prototype.altered_ = function(plan, index) {
-  if(index == 0) {
-    return plan[index];
-  } else {
-    var lastEnd = ble.util.maxByWithin(ble.interval.endRank, plan, 0, index);
-    var interval = plan[index];
-    if(interval.start() > lastEnd) {
-      return interval.withStartAt(lastEnd);
-    } else {
-      return interval;
-    }
-  }
+_.shiftBy = function(interval, amount) {
+  interval.withStartAt(interval.start() + amount);
 };
+
+_.startRank = function(x) { return x.start(); };
+_.endRank = function(x) { return x.end(); };
 
 /**
  * @constructor
  * @param {Array.<ble.interval.Interval>} data
  */
-ble.interval.Fetcher = function(data) {
+_.Fetcher = function(data) {
   this.byEnd = data.slice();
   this.byEnd.sort(this.comparator_);
 };
@@ -184,19 +159,19 @@ ble.interval.Fetcher = function(data) {
 /**
  * @const
  */
-ble.interval.Fetcher.prototype.comparator_ =
-  ble.util.comparatorFromRank(ble.interval.endRank);
+_.Fetcher.prototype.comparator_ =
+  ble.util.comparatorFromRank(_.endRank);
 /**
  * @const
  */
-ble.interval.Fetcher.prototype.rank_ = ble.interval.endRank;
+_.Fetcher.prototype.rank_ = _.endRank;
 
 
 /**
  * @param {number} point
  * @return {Array.<Array.<ble.interval.Interval>>}
  */
-ble.interval.Fetcher.prototype.beforeAndDuring = function(point) {
+_.Fetcher.prototype.beforeAndDuring = function(point) {
   var n = this.byEnd.length;
   var lastBefore =
     Math.ceil(ble.util.rankBinarySearch(this.rank_, this.byEnd, point));
@@ -211,18 +186,22 @@ ble.interval.Fetcher.prototype.beforeAndDuring = function(point) {
 /**
  * @param {ble.interval.Interval} interval
  */
-ble.interval.Fetcher.prototype.add = function(interval) {
+_.Fetcher.prototype.add = function(interval) {
   ble.util.rankBinaryInsert(this.rank_, this.byEnd, interval);
 };
 
-ble.interval.makeRandom = function(samples, size) {
+_.makeRandom = function(samples, size) {
   var result = [];
   for(var i = 0; i < samples; i++) {
     var a = Math.round(Math.random() * size),
         b = Math.round(Math.random() * size),
         start = Math.min(a, b),
         end = Math.max(Math.max(a, b), start + 1);
-    result.push(new ble.interval.Impl(start, end)); 
+    result.push(new _.Impl(start, end)); 
   }
   return result;
 }; 
+
+////////////////////////////////////////////////////////////////////////////////
+                                                                           });
+////////////////////////////////////////////////////////////////////////////////
